617d867377ca0a88dda0abf9b57ce4f7d4eb89a7
zainvice <amazwings@gmail.com>
2023-11-19 08:46:33 +0500

    Update v1.0

--- config/allowedOrgins.js
+++ config/allowedOrgins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:3000',
     'http://localhost:3001',
-    'https://main.d2uw14r4mivh3i.amplifyapp.com/',
+    'https://main.d2uw14r4mivh3i.amplifyapp.com',
     ''
     
 ]
